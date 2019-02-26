8e6af32a3c85749b91ff245ba7b917e1b0f69874
darinorange <OrangeHeroDavid@163.com>
2019-02-26 20:58:20 +0800

    update
    update

--- static/js/ajax_setup.js
+++ static/js/ajax_setup.js
@@ -21,6 +21,7 @@ function csrfSafeMethod(method) {
   return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
 
+
 $.ajaxSetup({
   beforeSend: function (xhr, settings) {
     if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
